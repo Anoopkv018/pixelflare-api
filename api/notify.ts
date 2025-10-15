9971ae96d1f11dfdf65862bba435a44ecd7ccfc1
Anoop K V <anuvirat18@gmail.com>
2025-10-16 00:39:02 +0530

    bb

--- api/notify.ts
+++ api/notify.ts
@@ -2,7 +2,7 @@
 import nodemailer from "nodemailer";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req: any, res: any) {
   res.setHeader("Access-Control-Allow-Origin", process.env.ALLOW_ORIGIN || "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
